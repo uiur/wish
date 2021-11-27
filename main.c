c189a5989792d6b568b41d16737710b101a001cd
Kazato Sugimoto <uiureo@gmail.com>
2021-11-27 16:15:35 +0900

    Launch process

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/errno.h>
+
+#define TOKEN_BUF_SIZE 100
+#define FILE_PATH_BUF_SIZE 100
 
 char *paths[] = {
   "",
@@ -10,11 +14,12 @@ char *paths[] = {
 };
 
 char* search_paths(const char* command) {
-  char* buf = malloc(sizeof(char) * 100);
+  int buf_size = FILE_PATH_BUF_SIZE;
+  char* buf = malloc(sizeof(char) * buf_size);
 
   for (int i = 0; paths[i] != NULL; i++) {
-    strlcpy(buf, paths[i], sizeof(buf));
-    strlcat(buf, command, sizeof(buf));
+    strlcpy(buf, paths[i], buf_size);
+    strlcat(buf, command, buf_size);
     int status = access(buf, X_OK);
     if (status == 0) {
       return buf;
@@ -24,53 +29,60 @@ char* search_paths(const char* command) {
   return NULL;
 }
 
+void execute(char* const args[]) {
+  int pid = fork();
+  if (pid < 0) {
+    fprintf(stderr, "fork failed\n");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) {
+    char* file_path = search_paths(args[0]);
+    if (file_path == NULL) {
+      fprintf(stderr, "command not found: %s\n", args[0]);
+      exit(EXIT_FAILURE);
+    }
+    int rc_exec = execv(file_path, args);
+    if (rc_exec == -1) {
+      fprintf(stderr, "exec failed: %s\n", file_path);
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+    exit(0);
+  } else {
+    int rc_wait = wait(NULL);
+    if (rc_wait == -1) {
+      fprintf(stderr, "child failed:\n");
+      perror(NULL);
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
   while (1) {
     char* line = NULL;
     size_t linecap = 0;
     ssize_t linelen;
 
-    printf("wish> ");
-    linelen = getline(&line, &linecap, stdin);
-    if (linelen <= 0) exit(0);
-    line[linelen - 1] = '\0';
+    printf("> ");
+    if (getline(&line, &linecap, stdin) < 0) exit(EXIT_FAILURE);
+    if (strcmp(line, "exit\n") == 0) exit(0);
 
-    if (strcmp(line, "exit") == 0) exit(0);
-
-    char* str = strdup(line);
-    char* to_free = str;
-
-    char** args = calloc(0, sizeof(char*));
+    int buf_size = TOKEN_BUF_SIZE;
+    char** args = calloc(buf_size, sizeof(char*));
     int args_size = 0;
     char* token;
-    while ((token = strsep(&str, " ")) != NULL) {
+    while ((token = strsep(&line, " \t\n"))) {
+      if (strlen(token) == 0) continue;
+
+      args[args_size] = token;
       args_size++;
-      args = realloc(args, args_size * sizeof(char*));
-      args[args_size - 1] = token;
+      if (args_size >= buf_size) {
+        buf_size += TOKEN_BUF_SIZE;
+        args = realloc(args, buf_size * sizeof(char*));
+      }
+      if (token == NULL) break;
     }
 
-    int rc = fork();
-    if (rc < 0) {
-      fprintf(stderr, "fork failed\n");
-      exit(1);
-    } else if (rc == 0) {
-      // child
-      char* file_path = search_paths(args[0]);
-      if (file_path == NULL) {
-        fprintf(stderr, "command not found: %s\n", args[0]);
-        exit(1);
-      }
-      int rc_exec = execv(file_path, args);
-      if (rc_exec == -1) {
-        fprintf(stderr, "exec failed\n");
-      }
-      exit(0);
-    } else {
-      // parent
-      int rc_wait = wait(NULL);
-    }
-
-    free(to_free);
+    execute(args);
 
     free(line);
   }
